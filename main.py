4871d0e7e5dca9a5eb8d7dc82c3ecd01f4ca7a67
Osser Shukirbai <oser.shukerbai2005@gmail.com>
2024-07-10 14:50:34 +0500

    third commit

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from typing import Tuple
 print("Helllo world")
 
 def foo(x: int) -> Tuple[int, float]:
-    return x, math.pi
+    return x, math.pi+1
 
 pprint.pprint("Hello")
 
 print(foo(4))
 print(foo(5))
+#wtf
